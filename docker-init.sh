0fba53a737002347d5ea3f50ae72cce8b588123f
Matthew Burke <Burke9077@gmail.com>
2021-11-30 18:48:45 -0500

    Update docker-init.sh

--- docker-init.sh
+++ docker-init.sh
@@ -3,6 +3,7 @@
 # Setup Cosmovisor
 echo "export DAEMON_NAME=gaiad" >> ~/.profile
 echo "export DAEMON_HOME=/root/.gaia" >> ~/.profile
+echo "export DAEMON_ALLOW_DOWNLOAD_BINARIES=true" >> ~/.profile
 source ~/.profile
 
 # Install cosmovisor
@@ -10,4 +11,4 @@ mkdir -p ~/.gaia/cosmovisor/upgrades
 mkdir -p ~/.gaia/cosmovisor/genesis/bin/
 cp $(which gaiad) ~/.gaia/cosmovisor/genesis/bin/
 
-sleep infinity
+cosmovisor start
